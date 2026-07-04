82ed2f724e911b3f03249e67d94fa22e6a8b639d
Michael Banzon <mrnbanzon@gmail.com>
2026-07-03 22:26:07 -0700

    feat: :sparkles: adding basic middleware to basic express server

--- basic-express-server/server.mjs
+++ basic-express-server/server.mjs
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+// basic middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
+  next();
+});
+
 // basic route
 app.get('/', (req, res, next) => {
   res.send('Hello World!');
