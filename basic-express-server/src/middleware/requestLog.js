adf82bc69694d40aa4e1ba5b0285186ed8501077
Michael Banzon <mrnbanzon@gmail.com>
2026-07-07 10:09:27 -0700

    feat: :sparkles: updated requestLog middleware to include logging elapsed response time

--- basic-express-server/src/middleware/requestLog.js
+++ basic-express-server/src/middleware/requestLog.js
@@ -1,4 +1,14 @@
 export default (req, res, next) => {
-  console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
+  const startTimeNano = process.hrtime.bigint();
+  const startTimeMilli = Number(startTimeNano / 1000000n); // Convert nanoseconds to milliseconds
+
+  console.log(`${req.method} ${req.url} - ${new Date(startTimeMilli).toISOString()}`);
+  res.on('finish', () => {
+    const endTimeNano = process.hrtime.bigint();
+    const elapsedTimeNano = endTimeNano - startTimeNano;
+
+    const elapsedTimeMilli = Number(elapsedTimeNano / 1000000n); // Convert nanoseconds to milliseconds
+    console.log(`${req.method} ${req.url} - ${new Date(startTimeMilli).toISOString()} Response time: ${elapsedTimeMilli} ms`);
+  });
   next();
 };
