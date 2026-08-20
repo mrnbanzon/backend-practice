01be8df37f23e822572902e9d82cd42d36e934cb
Michael Banzon <mrnbanzon@gmail.com>
2026-08-20 14:33:10 -0700

    fix: expire cached endpoint result in 1 min

--- cache-aside-example/src/modules/posts/posts.controller.js
+++ cache-aside-example/src/modules/posts/posts.controller.js
@@ -19,7 +19,7 @@ const getPosts = async (req, res, next) => {
     const result = await fetchPosts({ cursor, limit: Number(limit) });
 
     // cache result fetched from db in redis (stringify)
-    await redis.set(cacheKey, JSON.stringify(result), 120); // expire: 2 min
+    await redis.set(cacheKey, JSON.stringify(result), 60); // expire: 1 min
 
     return res.json(result);
   } catch (err) {
