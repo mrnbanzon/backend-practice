8ed19bed805b7df86887efa2c28f6678d4945ea1
Michael Banzon <mrnbanzon@gmail.com>
2026-08-25 13:07:52 -0700

    feat: :sparkles: adding crud functionality to basic server 3

--- basic-server-3/src/controllers/productController.js
+++ basic-server-3/src/controllers/productController.js
@@ -1,23 +1,60 @@
 import Product from "../models/Product.js";
 
-const createProduct = (req, res, next) => {
-  console.log('creating product');
-  res.end();
+const createProduct = async (req, res, next) => {
+  try {
+    const { name, price, category } = req.body;
+    const newProduct = new Product({
+      name,
+      price,
+      category,
+    });
+
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const getAllProducts = (req, res, next) => {
-  console.log('fetching all products');
-  res.end();
+const getAllProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find().lean();
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const updateProduct = (req, res, next) => {
-  console.log('updating product');
-  res.end();
+const updateProduct = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, price, category } = req.body;
+
+    const existing = await Product.findById(id);
+    if (!existing) {
+      return res.status(404).send('Product not found.');
+    }
+
+    existing.name = name || existing.name;
+    existing.price = price || existing.price;
+    existing.category = category || existing.category;
+
+    await existing.save();
+    res.json(existing);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const deleteProduct = (req, res, next) => {
-  console.log('deleting product');
-  res.end();
+const deleteProduct = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
 };
 
 export {
--- basic-server-3/src/models/Product.js
+++ basic-server-3/src/models/Product.js
@@ -1,9 +1,29 @@
 import { Schema, model } from 'mongoose';
 
 const productSchema = new Schema({
-  name: String,
-  price: Number,
-  category: String,
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    trim: true,
+    index: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: null
+  }
 });
 
 export default model('Product', productSchema);
